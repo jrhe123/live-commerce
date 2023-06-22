6f3be49b3f6aa0924d945849f0a946c662a8db07
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-06-21 21:54:01 -0400

    spring implement mysql master & slave data replication

--- docker/mysql/slave.sql
+++ docker/mysql/slave.sql
@@ -6,7 +6,7 @@ stop slave;
 reset slave;
 
 -- 157 is from "show master status;"
-change master to master_host='192.168.12.7',master_port=8808,master_user='test-slave',master_password='test-81710181',master_log_file='mysql-bin.000001',master_log_pos=157;
+change master to master_host='192.168.12.5',master_port=8808,master_user='test-slave',master_password='test-81710181',master_log_file='mysql-bin.000001',master_log_pos=157;
 
 start slave;
 
@@ -16,3 +16,8 @@ start slave;
 
 show slave status;
 -- check "Slave_IO_Running" & "Slave_SQL_Running" if they're "Yes", means working
+
+-- if it's not working, after reboot, try below
+STOP SLAVE;
+SET GLOBAL SQL_SLAVE_SKIP_COUNTER = 1; 
+START SLAVE;
