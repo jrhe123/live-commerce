f42eed03200fe6561d231f2dc6fa3def5b14a27d
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-06-22 21:56:54 -0400

    add redis in spring

--- live-app/live-user-provider/src/main/java/org/example/live/user/provider/service/impl/UserServiceImpl.java
+++ live-app/live-user-provider/src/main/java/org/example/live/user/provider/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import org.example.live.user.dto.UserDTO;
 import org.example.live.user.provider.dao.mapper.IUserMapper;
 import org.example.live.user.provider.dao.po.UserPO;
 import org.example.live.user.provider.service.IUserService;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import jakarta.annotation.Resource;
@@ -14,15 +15,27 @@ public class UserServiceImpl implements IUserService {
 
 	@Resource
 	private IUserMapper userMapper;
+	
+	@Resource
+	private RedisTemplate<String, UserDTO> redisTemplate;
 
 	@Override
 	public UserDTO getByUserId(Long userId) {
 		if (userId == null) {
 			return null;
 		}
-		
-		return ConvertBeanUtils.convert(
+		// redis
+		String keyString = "userInfo:" + userId;
+		UserDTO userDTO = redisTemplate.opsForValue().get(keyString);
+		if (userDTO != null) return userDTO;
+		// mysql
+		userDTO = ConvertBeanUtils.convert(
 				userMapper.selectById(userId), UserDTO.class);
+		if (userDTO != null) {
+			redisTemplate.opsForValue().set(keyString, userDTO);
+		}
+		
+		return userDTO;
 	}
 
 	@Override
