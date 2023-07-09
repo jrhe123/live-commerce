c5da534696627fd7b11c0aeab0acb9c6c6ceebb4
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-09 12:30:59 -0400

    impl account service

--- live-app/live-account-provider/src/main/java/org/example/live/accout/provider/service/impl/AccountTokenServiceImpl.java
+++ live-app/live-account-provider/src/main/java/org/example/live/accout/provider/service/impl/AccountTokenServiceImpl.java
@@ -1,6 +1,10 @@
 package org.example.live.accout.provider.service.impl;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
 import org.example.live.accout.provider.service.IAccountTokenService;
+import org.example.live.framework.redis.starter.key.AccountProviderCacheKeyBuilder;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
@@ -13,18 +17,23 @@ public class AccountTokenServiceImpl implements IAccountTokenService {
 	private RedisTemplate<String, Object> redisTemplate;
 	
 	@Resource
-	
+	private AccountProviderCacheKeyBuilder cacheKeyBuilder;
 
 	@Override
 	public String createAndSaveLoginToken(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		String tokenString = UUID.randomUUID().toString();
+		String redisKey = cacheKeyBuilder.buildUserLoginTokenKey(tokenString);
+		redisTemplate.opsForValue().set(redisKey, String.valueOf(userId), 
+				30, TimeUnit.DAYS);
+		return tokenString;
 	}
 
 	@Override
 	public Long getUserIdByToken(String tokenKey) {
-		// TODO Auto-generated method stub
-		return null;
+		String redisKey = cacheKeyBuilder.buildUserLoginTokenKey(tokenKey);
+		Integer userId = (Integer) redisTemplate.opsForValue().get(redisKey);
+		
+		return userId == null ? null : Long.valueOf(userId);
 	}
 
 }
