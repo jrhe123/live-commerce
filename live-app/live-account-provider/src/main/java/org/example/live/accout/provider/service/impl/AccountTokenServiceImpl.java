87dfa8d8683038ec054c2daf593860d822e1c316
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-09 12:25:33 -0400

    add redis key builder for account module

--- live-app/live-account-provider/src/main/java/org/example/live/accout/provider/service/impl/AccountTokenServiceImpl.java
+++ live-app/live-account-provider/src/main/java/org/example/live/accout/provider/service/impl/AccountTokenServiceImpl.java
@@ -1,10 +1,19 @@
 package org.example.live.accout.provider.service.impl;
 
 import org.example.live.accout.provider.service.IAccountTokenService;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
+
+import jakarta.annotation.Resource;
 
 @Service
 public class AccountTokenServiceImpl implements IAccountTokenService {
+	
+	@Resource
+	private RedisTemplate<String, Object> redisTemplate;
+	
+	@Resource
+	
 
 	@Override
 	public String createAndSaveLoginToken(Long userId) {
