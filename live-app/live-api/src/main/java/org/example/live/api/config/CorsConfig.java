52149b0cbd1edd667bb07fba24738b6536149d38
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-09 19:28:55 -0400

    move cors to gateway

--- live-app/live-api/src/main/java/org/example/live/api/config/CorsConfig.java
+++ live-app/live-api/src/main/java/org/example/live/api/config/CorsConfig.java
@@ -9,29 +9,37 @@ import org.springframework.web.filter.CorsFilter;
 import java.util.ArrayList;
 import java.util.List;
 
-@Configuration
+/**
+ * 
+ * moved the cors config to GATEWAY
+ * 
+ * @author jiaronghe
+ *
+ */
+
+//@Configuration
 public class CorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", buildConfig()); // cors config
-        return new CorsFilter(source);
-    }
-
-    private CorsConfiguration buildConfig() {
-        CorsConfiguration corsConfiguration = new CorsConfiguration();
-        // session id multiple access
-        // save token into cookie
-        corsConfiguration.setAllowCredentials(true);
-        // allow hosts
-        List<String> allowedOriginPatterns = new ArrayList<>();
-        allowedOriginPatterns.add("*");
-        corsConfiguration.setAllowedOriginPatterns(allowedOriginPatterns);
-        // allow headers
-        corsConfiguration.addAllowedHeader("*");
-        // allow all methods（post, get...）
-        corsConfiguration.addAllowedMethod("*");
-        return corsConfiguration;
-    }
+//    @Bean
+//    public CorsFilter corsFilter() {
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", buildConfig()); // cors config
+//        return new CorsFilter(source);
+//    }
+//
+//    private CorsConfiguration buildConfig() {
+//        CorsConfiguration corsConfiguration = new CorsConfiguration();
+//        // session id multiple access
+//        // save token into cookie
+//        corsConfiguration.setAllowCredentials(true);
+//        // allow hosts
+//        List<String> allowedOriginPatterns = new ArrayList<>();
+//        allowedOriginPatterns.add("*");
+//        corsConfiguration.setAllowedOriginPatterns(allowedOriginPatterns);
+//        // allow headers
+//        corsConfiguration.addAllowedHeader("*");
+//        // allow all methods（post, get...）
+//        corsConfiguration.addAllowedMethod("*");
+//        return corsConfiguration;
+//    }
 }
