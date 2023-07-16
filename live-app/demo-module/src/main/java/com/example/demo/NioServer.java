b66bb5470e9a1bcb6ec61d8335156589f18af1cb
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-16 12:07:49 -0400

    add nio example

---
+++ live-app/demo-module/src/main/java/com/example/demo/NioServer.java
@@ -0,0 +1,61 @@
+package com.example.demo;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class NioServer {
+	
+	private static List<SocketChannel> acceptSocketList = new ArrayList<>();
+
+	public static void main(String[] args) throws IOException {
+		
+		ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+		// bind to 9090, and config non-blocking
+		serverSocketChannel.socket().bind(new InetSocketAddress(9090));
+		serverSocketChannel.configureBlocking(false);
+		
+		
+		// keep reading data, even it's empty "byteBuffer"
+		// Notes: if we have 10,000 socket in the list, it will cause the high CPU usage issue
+		new Thread(() -> {
+			while(true) {
+				for (SocketChannel socketChannel : acceptSocketList) {
+					try {
+						
+						ByteBuffer byteBuffer = ByteBuffer.allocate(10);
+						socketChannel.read(byteBuffer);
+						System.out.println("server recived data: " + 
+								new String(byteBuffer.array()));
+						
+					} catch (IOException e) {
+						e.printStackTrace();
+					}					
+				}
+				
+				
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+		
+		
+		// async listening here
+		while(true) {
+			// accept - nio accept is non-blocking
+			SocketChannel socketChannel = serverSocketChannel.accept();
+			if (socketChannel != null) {
+				System.out.println(">>>>> connection built");
+				socketChannel.configureBlocking(false);
+				acceptSocketList.add(socketChannel);
+			}
+		}
+	}
+}
