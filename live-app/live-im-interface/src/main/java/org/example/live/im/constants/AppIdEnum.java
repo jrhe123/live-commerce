079d4df5581a143bd079afd31d39c85a3cf4a5c6
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-22 21:35:52 -0400

    add yaml to nacos & test it with command link

---
+++ live-app/live-im-interface/src/main/java/org/example/live/im/constants/AppIdEnum.java
@@ -0,0 +1,26 @@
+package org.example.live.im.constants;
+
+public enum AppIdEnum {
+
+	LIVE_BIZ_APP(1, "live stream app");
+	
+	int code;
+	String desc;
+	
+	AppIdEnum(int code, String desc) {
+		this.code = code;
+		this.desc = desc;
+	}
+
+	public int getCode() {
+		return code;
+	}
+
+
+	public String getDesc() {
+		return desc;
+	}
+
+	
+	
+}
--- live-app/live-im-provider/src/main/java/org/example/live/im/provider/ImProviderApplication.java
+++ live-app/live-im-provider/src/main/java/org/example/live/im/provider/ImProviderApplication.java
@@ -1,14 +1,22 @@
 package org.example.live.im.provider;
 
 import org.apache.dubbo.config.spring.context.annotation.EnableDubbo;
+import org.example.live.im.constants.AppIdEnum;
+import org.example.live.im.provider.service.ImTokenService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.Resource;
+
 
 @SpringBootApplication
 @EnableDubbo
-public class ImProviderApplication {
+public class ImProviderApplication implements CommandLineRunner {
+	
+	@Resource
+	private ImTokenService imTokenService;
 
 	
 	public static void main(String[] args) {
@@ -18,5 +26,19 @@ public class ImProviderApplication {
         springApplication.setWebApplicationType(WebApplicationType.NONE);
         springApplication.run(args);
     }
+
+	@Override
+	public void run(String... args) throws Exception {
+		long userId = 109213231L;
+		
+		String token = imTokenService.cressteImLoginToken(
+				userId, AppIdEnum.LIVE_BIZ_APP.getCode());
+		
+		Long userIdByToken = imTokenService.getUserIdByToken(token);
+		
+		System.out.println(">>>>>>>");
+		System.out.println(">>>>>>> userId: " + userId);
+		System.out.println(">>>>>>> retrived from token: " + userIdByToken);
+	}
 
 }
