1415cc1c7c4a385cd66add73c70aba25b0500b9f
Jiarong He <jiaronghe@Jiarongs-MacBook-Pro.local>
2023-07-22 21:25:46 -0400

    impl im token service

--- live-app/live-im-provider/src/main/java/org/example/live/im/provider/service/impl/ImTokenServiceImpl.java
+++ live-app/live-im-provider/src/main/java/org/example/live/im/provider/service/impl/ImTokenServiceImpl.java
@@ -1,5 +1,9 @@
 package org.example.live.im.provider.service.impl;
 
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import org.example.live.framework.redis.starter.key.ImProviderCacheKeyBuilder;
 import org.example.live.im.provider.service.ImTokenService;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
@@ -12,17 +16,27 @@ public class ImTokenServiceImpl implements ImTokenService {
 	@Resource
 	private RedisTemplate<String, Object> redisTemplate;
 	
+	@Resource
+	private ImProviderCacheKeyBuilder cacheKeyBuilder;
+	
 
 	@Override
 	public String cressteImLoginToken(long userId, int appId) {
-		// TODO Auto-generated method stub
-		return null;
+		String token = UUID.randomUUID() + "%" + appId;
+		String redisKey = cacheKeyBuilder.buildImLoginTokenKey(token);
+		
+		redisTemplate.opsForValue().set(redisKey, userId,
+				5, TimeUnit.MINUTES);
+		
+		return token;
 	}
 
 	@Override
 	public Long getUserIdByToken(String token) {
-		// TODO Auto-generated method stub
-		return null;
+		String redisKey = cacheKeyBuilder.buildImLoginTokenKey(token);
+		Object userId = redisTemplate.opsForValue().get(redisKey);
+		
+		return userId == null ? null : Long.valueOf((Integer) userId);
 	}
 
 }
